caa46afcc29e2ec5436ee2a1e57e99814df6ba16
MNM-Hussain <mnmhuss91@gmail.com>
2021-08-19 20:14:56 +0530

    Insert function for Subject Notices in frontend completed

--- Server/routes/TeacherRoutes.js
+++ Server/routes/TeacherRoutes.js
@@ -3,7 +3,7 @@ const SubjectNotice = require('../model/SubjectNotice.js')
 const SubjectMaterial = require('../model/SubjectMaterials')
 
 const router = express.Router();
-
+//***********Crud for Subject Notices**********************************
 //insert the subject notices
 router.post('/insertSubjectNotices',async (req,res) => {
     const body = req.body;
@@ -15,7 +15,17 @@ router.post('/insertSubjectNotices',async (req,res) => {
         console.log(e)
     }
 })
+//retrieve the subject notices
+router.get('/getSubjectNotices',async (req,res) => {
+    try {
+        const subjectNotices = await SubjectNotice.find();
+        res.send({data:subjectNotices,success:true})
+    }catch (e) {
+        console.log(e)
+    }
+})
 
+//**************crud for Subject Materials****************************************************
 //insert the Subject Materials
 router.post('/insertSubjectMaterials',async (req,res) => {
     const body = req.body;
@@ -23,6 +33,15 @@ router.post('/insertSubjectMaterials',async (req,res) => {
         const subjectMaterial = new SubjectMaterial(body);
         await subjectMaterial.save();
         res.send({success:true})
+    }catch (e) {
+        console.log(e)
+    }
+})
+//retrieve the subject Materials
+router.get('/getSubjectMaterials',async (req,res) => {
+    try {
+        const subjectMaterials = await SubjectMaterial.find();
+        res.send({data:subjectMaterials,success:true})
     }catch (e) {
         console.log(e)
     }
