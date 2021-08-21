93c6a4141bd4ad660cd1fd081103bd31c14a68e3
MNM-Hussain <mnmhuss91@gmail.com>
2021-08-21 12:20:09 +0530

    Frontend Retrieve part completed on MathematicsSubjectView and MathematicsTeachersView

--- client/src/components/Teacher/SubjectMaterial/TeacherSubjectMaterialRetreive/MathematicsTeachersView.jsx
+++ client/src/components/Teacher/SubjectMaterial/TeacherSubjectMaterialRetreive/MathematicsTeachersView.jsx
@@ -20,17 +20,17 @@ class MathematicsTeachersView extends Component {
         TeacherMaterials:[]
     }
 
-    componentDidMount = async ()=> {
+    componentDidMount = async () => {
         await  axios.get('http://localhost:5000/teacher/getSubjectNotices').
         then(res => {
-            const  TeacherNotices = res.data;
-            console.log("Damn" + TeacherNotices);
+            const  TeacherNotices = res.data.data;
+            console.log("Damn" + res.data.data);
             this.setState({TeacherNotices: TeacherNotices});
         }).catch(err => err.message)
 
         await axios.get('http://localhost:5000/teacher/getSubjectMaterials').
         then(res => {
-            const  TeacherMaterials = res.data;
+            const  TeacherMaterials = res.data.data;
             console.log("Bull" + TeacherMaterials);
             this.setState({TeacherMaterials: TeacherMaterials});
         }).catch(err => err.message)
@@ -63,20 +63,18 @@ class MathematicsTeachersView extends Component {
                             </Col>
                         </Row>
                     </div>
-                    {/***********************************Notice box ********************************/}
-                    {/*{this.state.Notices.map((Notices) => (*/}
+                    {/***********************************Notice box ********************************/}<>
+                    {this.state.TeacherNotices.map((Notices) => (
                     <div style={{width:"80%",marginLeft:"10%",marginTop:"50px",
                         border:"#ff3333", borderWidth:"3px", borderStyle:"solid", padding:"2%",
                         boxShadow: "0 1rem 2rem rgba(0,0,0,0.2)"}}>
 
                         <Typography variant="h6" style={{textAlign:"Center", textDecorationLine: 'underline', color:'#ff3333'}} gutterBottom>
-                            Notices to All Students
-                            {/*{Notices.noticeHeading}*/}
+                            {Notices.noticeHeading}
                         </Typography>
 
                         <Typography variant="subtitle1" gutterBottom>
-                            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                            {/*{Notices.noticeDetails}*/}
+                            {Notices.noticeDetails}
                         </Typography>
                         <Row>
                             <Col>
@@ -104,7 +102,8 @@ class MathematicsTeachersView extends Component {
                             </Col>
                         </Row>
                     </div>
-                    {/*))}*/}
+                    ))}
+                </>
                     {/************************Insert Buttons**************************************/}
                     <div style={{marginTop:"30px"}}>
                         <Row>
@@ -181,9 +180,12 @@ class MathematicsTeachersView extends Component {
                               border:"black",
                               borderStyle:"solid",
                               borderWidth:"1px"}}>
+
+                        {this.state.TeacherMaterials.map((Materials) => (
+                            <>
                         <Card
                             type="inner"
-                            title="Week 01"
+                            title=   {Materials.week}
                             style={{border:"#527a7a",
                                 borderStyle:"solid",
                                 borderWidth:"1px"}}
@@ -192,7 +194,7 @@ class MathematicsTeachersView extends Component {
                                 orientation="left"
                                 style={{fontSize:"22px"}}
                             >
-                                This week Lesson Fraction
+                                {Materials.unitName}
                             </Divider>
 
                             <Typography variant="h6" style={{textAlign:"left"}} gutterBottom>
@@ -204,7 +206,7 @@ class MathematicsTeachersView extends Component {
                             <Typography variant="h6" style={{textAlign:"left"}} gutterBottom>
                                 <PlayCircleOutlineIcon/>
                                 {" "}
-                                Lecture Link
+                                {Materials.lectureLink}
                             </Typography>
 
                             <Row>
@@ -232,69 +234,14 @@ class MathematicsTeachersView extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-
                         </Card>
-                        <Divider style={{border:"#527a7a",
+                            <Divider style={{border:"#527a7a",
                             borderStyle:"solid",
                             borderWidth:"1px"}}
-                        />
-                        <Card
-                            type="inner"
-                            title="Week 02"
-                            style={{border:"#527a7a",
-                                borderStyle:"solid",
-                                borderWidth:"1px",
-                                marginTop: 16
-                            }}
-
-                        >
-                            <Divider
-                                orientation="left"
-                                style={{fontSize:"22px"}}
-                            >
-                                This week Lesson Algebra
-                            </Divider>
-
-                            <Typography variant="h6" style={{textAlign:"left"}} gutterBottom>
-                                <PictureAsPdfIcon/>
-                                {" "}
-                                Lecture Material
-                            </Typography>
-
-                            <Typography variant="h6" style={{textAlign:"left"}} gutterBottom>
-                                <PlayCircleOutlineIcon/>
-                                {" "}
-                                Lecture Link
-                            </Typography>
-
-                            <Row>
-                                <Col>
-                                    <Button
-                                        variant="contained"
-                                        color="secondary"
-                                        // className={classes.button}
-                                        startIcon={<DeleteIcon />}
-                                        style={{marginLeft:"60%",width:"30%"}}
-                                    >
-                                        Delete
-                                    </Button>
-                                </Col>
-
-                                <Col>
-                                    <Button
-                                        variant="contained"
-                                        color="default"
-                                        // className={classes.button}
-                                        startIcon={<BorderColorIcon />}
-                                        style={{marginLeft:"10px",width:"30%",backgroundColor: "#282c34",color:"white"}}
-                                    >
-                                        Edit
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Card>
+                            />
+                            </>
+                        ))}
                     </Card>
-
                 </div>
             </>
         )
